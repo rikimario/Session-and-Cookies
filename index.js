c8f04a22a891046839e156665cd1042de4f8001e
rikimario <mario_martinov@abv.bg>
2023-10-05 00:23:21 +0300

    installing and using cookieParser

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const { v4: uuid } = require('uuid');
 const PORT = 5000;
 const app = express();
 
+app.use(cookieParser());
+
 
 app.get('/', (req, res) => {
-  res.send('ok!');
+  let id;
+  const userId = req.cookies['userId'];
+
+  if (userId) {
+    id = userId;
+    console.log(req.session);
+  } else {
+    id = uuid();
+    res.cookie('userId', id, { httpOnly: true });
+    res.cookie('userId2', id);
+  }
+
+  res.send('ok! ID: ' + id);
 });
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}...`));
