b51f4feb8bba4983db6b5075c263a4eb4ce39aeb
rikimario <mario_martinov@abv.bg>
2023-10-06 18:20:44 +0300

    express session

---
+++ index-express-cookie copy.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { v4: uuid } = require('uuid');
+const PORT = 5000;
+const app = express();
+
+
+app.get('/', (req, res) => {
+  let id;
+  const cookie = req.headers['cookie'];
+
+  if (cookie) {
+    // cookie first=asd;second=sada;third=asdkgsdf
+    const [key, value] = cookie.split('=');
+    id = value;
+    console.log({ key });
+    console.log({ value });
+  } else {
+    id = uuid();
+    res.header('Set-Cookie', `userId=${id}`);
+  }
+
+  res.send('ok! ID: ' + id);
+});
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}...`));
