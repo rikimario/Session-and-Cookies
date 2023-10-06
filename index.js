164c47ff64e157c49703ac3b366e0a2466a66348
rikimario <mario_martinov@abv.bg>
2023-10-06 20:53:56 +0300

    implement jwt and bcrypt

--- index.js
+++ index.js
@@ -1,30 +1,89 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const { v4: uuid } = require('uuid');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const PORT = 5000;
 const app = express();
 
-const session = {};
-
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }))
+
+const users = {};
 
 app.get('/', (req, res) => {
-  let id;
-  const userId = req.cookies['userId'];
+  const payload = { id: 123, username: 'Mario', age: 30 };
+  const secret = 'OurBiggestSecret';
+  const options = { expiresIn: '3d' };
+  //* synchronous code
+  const token = jwt.sign(payload, secret, options);
 
-  if (userId) {
-    id = userId;
-    console.log({ session });
+  res.send(token);
+});
+
+app.get('/verification/:token', (req, res) => {
+  const { token } = req.params;
+
+  // * synchronous code
+  const result = jwt.verify(token, 'OurBiggestSecret')
+  console.log({ result });
+  res.send('Ok!');
+});
+
+app.get('/login', (req, res) => {
+  res.send(`
+  <h3>Login</h3>
+  <form  method="post">
+  <label for="username">Username</label>
+  <input type="text" name="username">
+
+  <label for="password">Password</label>
+  <input type="password" name="password">
+
+  <input type="submit" value="Submit">
+</form>
+  `);
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const preservedHash = users[username]?.password;
+
+  // * removes the salt and compares pure hashes!
+  const isValid = await bcrypt.compare(password, preservedHash);
+
+  if (isValid) {
+    res.send('Successfully Authenticated!');
   } else {
-    id = uuid();
+    res.status(401).send('Unauthorized! :(');
 
-    session[id] = {
-      secret: 'my secret',
-    }
-    res.cookie('userId', id);
   }
 
-  res.send('ok! ID: ' + id);
+  res.send('Ok!');
+});
+
+app.get('/register', (req, res) => {
+  res.send(`
+  <h3>Register</h3>
+  <form  method="post">
+  <label for="username">Username</label>
+  <input type="text" name="username">
+
+  <label for="password">Password</label>
+  <input type="password" name="password">
+
+  <input type="submit" value="Submit">
+</form>
+  `);
+});
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  users[username] = { password: hash };
+
+  res.redirect('/login');
 });
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}...`));
